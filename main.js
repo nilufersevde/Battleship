c2516c2ac66356d3f8c3c588238f72eca5a33f7d
Nilufer <n.sevde@hotmail.com>
2023-12-24 13:29:24 +0300

    create the player.test.js

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";const t=new class{constructor(t){this.length=t,this.hits=0,this.sunk=!1}hit(){this.hits++}isSunk(){return this.length<=this.hits?(this.sunk=!0,!0):(console.log(this.length),console.log(this.hits),!1)}getShipLength(){return this.length}}(1);t.hit(),t.hit(),t.isSunk()})();
+(()=>{"use strict";const t=new class{constructor(t){this.length=t,this.hits=0,this.sunk=!1,this.coordinates=[]}hit(){this.hits++}isSunk(){return this.length<=this.hits&&(this.sunk=!0,!0)}getShipLength(){return this.length}}(1);t.hit(),t.hit(),t.isSunk()})();
